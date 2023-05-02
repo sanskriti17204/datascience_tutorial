bbec32fe01cc6467ab8acdef484a7cedfdfe9e62
sanskriti17204 <114944346+sanskriti17204@users.noreply.github.com>
2023-05-02 22:02:36 +0530

    Update and rename time_comp.py to timeSPACE_comp.py

--- timeSPACE_comp.py
+++ timeSPACE_comp.py
@@ -1,5 +1,6 @@
 from time import process_time
 import numpy as np
+import sys
 
 
 #to calcuate time taken by a list to process
@@ -16,3 +17,9 @@ start_time=process_time()
 np_array += 5
 end_time=process_time()
 print(end_time-start_time)
+
+
+py_arr=[0,4,15,98]
+np_arr=np.array(py_arr)
+print(sys.getsizeof(1)*len(py_arr))   #112
+print(np_arr.itemsize*np_arr.size)   #32
